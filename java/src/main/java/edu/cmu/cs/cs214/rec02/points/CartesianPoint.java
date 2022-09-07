c40686155aa26af30b968670009d1c393d3012d7
pariah2435 <44264819+pariah2435@users.noreply.github.com>
2022-09-07 10:58:54 -0400

    Access control

--- java/src/main/java/edu/cmu/cs/cs214/rec02/points/CartesianPoint.java
+++ java/src/main/java/edu/cmu/cs/cs214/rec02/points/CartesianPoint.java
@@ -1,8 +1,8 @@
 package edu.cmu.cs.cs214.rec02.points;
 
 public class CartesianPoint implements Point {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
     public CartesianPoint (int x, int y) {
         this.x = x;
